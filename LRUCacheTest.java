a91742c31fa3b3e87bc75d32752002f334eb0503
中田愛刀 <Aito-Nakata@users.noreply.github.com>
2021-05-13 15:09:20 +0900

    関数名の編集

--- LRUCacheTest.java
+++ LRUCacheTest.java
@@ -1,6 +1,6 @@
-public class LRUChacheTest{
+public class LRUCacheTest{
     //Test(1つも使われていない場合、追加したものから消えるようにする)
-    public void testLRU_access{
+    public void testLRU_access(){
         LRUCache<String> lru = new LRUCache<>();
         lru.put("a","dataA");
         lru.put("b","dataB");
@@ -9,7 +9,7 @@ public class LRUChacheTest{
     }
 
     //Test(getされたら使われたとみなすようにする)
-    public void testLRU_use{
+    public void testLRU_use(){
         LRUCache<String> lru = new LRUCache<>();
         lru.put("a","dataA");
         lru.put("b","dataB");
