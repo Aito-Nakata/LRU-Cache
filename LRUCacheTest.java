243ab84a03605ab694fe907eb2f142f41f697862
中田愛刀 <Aito-Nakata@users.noreply.github.com>
2021-05-13 15:19:10 +0900

    クラス名の変更

--- LRUCacheTest.java
+++ LRUCacheTest.java
@@ -1,7 +1,8 @@
+import java.util.Optional;
 public class LRUCacheTest{
     //Test(1つも使われていない場合、追加したものから消えるようにする)
     public void testLRU_access(){
-        LRUCache<String> lru = new LRUCache<>();
+        lru<String> lru = new LRUCache<>();
         lru.put("a","dataA");
         lru.put("b","dataB");
         lru.put("c","dataC");
@@ -10,7 +11,7 @@ public class LRUCacheTest{
 
     //Test(getされたら使われたとみなすようにする)
     public void testLRU_use(){
-        LRUCache<String> lru = new LRUCache<>();
+        lru<String> lru = new LRUCache<>();
         lru.put("a","dataA");
         lru.put("b","dataB");
         assertEquals("dataA",lru.get("a"));
