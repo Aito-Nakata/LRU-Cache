9b144e5955a6097e64df407cecf2bcd3a98452f2
中田愛刀 <Aito-Nakata@users.noreply.github.com>
2021-05-13 17:19:33 +0900

    データの参照の追加

--- LRUCache.java
+++ LRUCache.java
@@ -2,17 +2,22 @@ import java.util.HashMap;
 import java.util.Optional;
 public class LRUCache<T> implements lru<T> {
 
-    private HashMap<String, String> map = new HashMap<>(); //文字・キー共に文字列を格納
+    private HashMap<String, T> map = new HashMap<>(); //文字・キー共に文字列を格納
 
     //データの追加
     @Override
     public void put(String key, T value){
-
+        map.put(key,value);
     }
 
     //データの使用
     @Override
     public Optional<T> get(String key){
+        System.out.println(map.get(key));
+    }
 
+    //データの参照
+    public Optional<T> view_data(){
+        System.out.println(map.size());
     }
 }
